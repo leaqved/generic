0a3b81c5e7cc3a5d5f98f387108b2187038cfc17
leaqved <leaqved+dev@gmail.com>
2023-11-21 21:43:53 +0300

    feat(set): add constructor function

--- set/set.go
+++ set/set.go
@@ -6,3 +6,10 @@ type Set[T comparable] struct {
 	Mu   *sync.Mutex
 	Data map[T]struct{}
 }
+
+func New[T comparable]() *Set[T] {
+	return &Set[T]{
+		Mu:   &sync.Mutex{},
+		Data: make(map[T]struct{}),
+	}
+}
