082012fb1cf0eb3d042d675ab4485ab4cc1f01cf
leaqved <leaqved+dev@gmail.com>
2023-11-23 19:30:03 +0300

    fix(sln): make mutex private

--- list/singly/node.go
+++ list/singly/node.go
@@ -3,55 +3,55 @@ package singly
 import "sync"
 
 type Node[T any] struct {
-	Mu   *sync.Mutex
+	mu   *sync.Mutex
 	Val  T
 	Next *Node[T]
 }
 
 func New[T any]() *Node[T] {
 	return &Node[T]{
-		Mu: &sync.Mutex{},
+		mu: &sync.Mutex{},
 	}
 }
 
 func (n *Node[T]) SetVal(val T) {
-	n.Mu.Lock()
+	n.mu.Lock()
 	n.Val = val
-	n.Mu.Unlock()
+	n.mu.Unlock()
 }
 
 func (n *Node[T]) SetNext(next *Node[T]) {
-	n.Mu.Lock()
+	n.mu.Lock()
 	n.Next = next
-	n.Mu.Unlock()
+	n.mu.Unlock()
 }
 
 func (n *Node[T]) LoadVal() T {
 	var val T
-	n.Mu.Lock()
+	n.mu.Lock()
 	val = n.Val
-	n.Mu.Unlock()
+	n.mu.Unlock()
 	return val
 }
 
 func (n *Node[T]) LoadNext() *Node[T] {
 	var next *Node[T]
-	n.Mu.Lock()
+	n.mu.Lock()
 	next = n.Next
-	n.Mu.Unlock()
+	n.mu.Unlock()
 	return next
 }
 
 func (n *Node[T]) SwapVal(val T) T {
-	n.Mu.Lock()
+	n.mu.Lock()
 	val, n.Val = n.Val, val
-	n.Mu.Unlock()
+	n.mu.Unlock()
 	return val
 }
 
 func (n *Node[T]) SwapNext(next *Node[T]) *Node[T] {
-	n.Mu.Lock()
+	n.mu.Lock()
 	next, n.Next = n.Next, next
-	n.Mu.Unlock()
+	n.mu.Unlock()
 	return next
 }
